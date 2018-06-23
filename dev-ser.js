d9589329d24ee6fa00e23ab1f9860e4b3fa1b7ff
Attila Satan <attila.satan@gmail.com>
2018-06-23 21:11:55 +0300

    simplification on dev server config

--- dev-ser.js
+++ dev-ser.js
@@ -6,21 +6,7 @@ const path = require('path');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: {
-    index:path.resolve(__dirname,'./')
-  },
-  quiet: false,
-  noInfo: false,
-  stats: {
-    assets: false,
-    colors: true,
-    version: false,
-    hash: false,
-    timings: false,
-    chunks: false,
-    chunkModules: false,
-  },
+  historyApiFallback: true,
 }).listen(3000, 'localhost', (err) => {
   if (err) {
     console.log(err);
